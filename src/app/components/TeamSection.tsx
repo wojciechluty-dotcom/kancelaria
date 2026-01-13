dd4680fc49ade1dbe5a3f049f6cb04d088a5fbe3
wojciechluty-dotcom <wojciech.luty@gmail.com>
2026-01-13 21:27:29 +0100

    Update TeamSection.tsx

--- src/app/components/TeamSection.tsx
+++ src/app/components/TeamSection.tsx
@@ -6,7 +6,7 @@ export function TeamSection() {
     {
       name: "Wojciech Luty",
       title: "Adwokat",
-      image: "/public/images/team-luty.png",
+      image: "/images/team-luty.png",
       phone: "+48 694 491 095",
       email: "wojciech.luty@wsla.pl",
       specializations: [
